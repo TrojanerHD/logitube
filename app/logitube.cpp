f1d6089e31c501f728d8a178ba60f774ba4d0707
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2022-05-15 03:10:03 +0200

    Add color with strength to hex function Deobfuscates the color resolution method

--- app/logitube.cpp
+++ app/logitube.cpp
@@ -18,21 +18,34 @@ std::string decimal_to_hexadecimal(int num) {
   return str; 
 }
 
+/**
+  * @brief Apply a strength to a color
+  * @param r The red component of the color (0 to 255)
+  * @param g The green component of the color (0 to 255)
+  * @param b The blue component of the color (0 to 255)
+  * @param strength The strength to apply (0 to 255)
+  * @return The resulting color as a hex string
+  * @example color(255, 12, 2, 127.5) => "7F0601"
+  */
+std::string color_with_strength_to_hex(int r, int g, int b, double strength) {
+  std::string red = decimal_to_hexadecimal(strength / 255 * r);
+  std::string green = decimal_to_hexadecimal(strength / 255 * g);
+  std::string blue = decimal_to_hexadecimal(strength / 255 * b);
+  return red + green + blue;
 }
 
 void set_key(int key, double strength, std::string service) {
   std::ostringstream oss;
-  std::string colorCode = decimal_to_hexadecimal(strength);
   oss << "g910-led -kn f" << key << " ";
 
   if (service == "youtube")
-    oss << colorCode << "0000";
+    oss << color_with_strength_to_hex(255, 0, 0, strength);
   else if (service == "youtube-ad")
-    oss << colorCode << decimal_to_hexadecimal(strength / 255 * 171) << "00";
+    oss << color_with_strength_to_hex(255, 171, 0, strength);
   else if (service == "disney")
-    oss << colorCode << colorCode << colorCode;
+    oss << color_with_strength_to_hex(255, 255, 255, strength);
   else if (service == "w2g")
-    oss << "00" << decimal_to_hexadecimal(strength / 255 * 96) << decimal_to_hexadecimal(strength / 255 * 223);
+    oss << color_with_strength_to_hex(0, 96, 223, strength);
 
   std::string command = oss.str();
   system(command.c_str());
